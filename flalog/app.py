0fdffe845c0951de2c41f3acbf6c59e644fc8435
Akio Ogawa <akio.ogawa02@gmail.com>
2024-02-08 19:05:28 +0900

    できるだけ単純にわかりやすく

--- flalog/app.py
+++ flalog/app.py
@@ -1,53 +1,61 @@
 from flask import Flask, url_for, render_template, redirect, request, flash
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
-from .user import my_user
+from .user import get_user
 
 app = Flask(__name__)
 # flaskのセッション用キー
 app.config['SECRET_KEY'] = 'secret-key-for-flask-session'
 
-# Flask-Login初期設定
+# Flask-Loginの初期化
 login_manager = LoginManager()
 login_manager.init_app(app)
 # ログインしていない時に送り返す先
 login_manager.login_view = 'login_gamen'
 # ログインしていない時に表示するメッセージ
-login_manager.login_message = '先にログインしてから来て！'
+login_manager.login_message = 'ログインしてないからログイン画面に戻します！'
 
-# Flask-Loginがユーザオブジェクトを取得したいときのコールバック
+
 @login_manager.user_loader
 def get_user(id):
-    # 普通はここでidをキーにDBとか参照する
-    return my_user if id == my_user.id else None
+    """
+    Flask-Loginからのコールバック
+    本当はDBとか見るけど、今回はユーザひとりなのでIDだけ合ってればよし
+    """
+    user = get_user()
+    return user if id == user.id else None 
 
-# ログイン画面表示
+@app.route('/')
 @app.route('/login_gamen')
 def login_gamen():
+    """ログイン画面の表示"""
     return render_template('login.html')
 
-# ログイン処理
 @app.route('/login', methods=['POST'])
 def login():
-    # 認証処理は自分でやる
-    if request.form['id'] == my_user.id and request.form['password'] == my_user.password:
-        # login_userを呼び出すとログインしたことになる
-        login_user(my_user) 
+    """
+    自分で認証して、login_user()を呼んでFlask-Loginにログインを伝える。
+    ※認証がただのif文なのでサンプルを見ても気付きにくい
+    """
+    user = get_user()
+    if request.form['id'] == user.id and request.form['password'] == user.password:
+        # Flask-Login的ログイン完了はここ
+        login_user(user) 
         return redirect(url_for('login_success'))
     else:
         # ログイン失敗 ログイン画面に戻す
         flash('ログインに失敗したよ！')
         return redirect(url_for('login_gamen'))
 
-# ログインしないと入れない画面
+# @login_required がログイン必須route
 @app.route('/login_success')
 @login_required
 def login_success():
-    # ログイン中のユーザはcurrent_userで取得可能
+    """ログイン中のユーザはcurrent_userで参照可能"""
     return render_template('login_success.html', id=current_user.id)
 
 # ログアウト
 @app.route('/logout', methods=['POST'])
 def logout():
-    # 現在のユーザをログアウトする
+    """ログアウト"""
     logout_user()
     return render_template('logout_success.html')
--- flalog/user.py
+++ flalog/user.py
@@ -1,11 +1,12 @@
 from flask_login import UserMixin
 
-# ユーザオブジェクトの例 とりあえずUserMixinは継承しておく
-# ほとんどの記事ではSQLAlchemyのModelとして構築しているけど、Flask-LoginとDBは関係ない
 class User(UserMixin):
+    """ユーザを表すオブジェクト"""
+    """説明用なのでDBに格納もしないし、パスワードのハッシュ化とかもなし"""
     def __init__(self ,id, password) -> None:
         self.id = id
         self.password = password
 
-# DBとか使わないでひとりだけユーザを作る
-my_user = User('myuser', 'mypassword')
+def get_user():
+    """ユーザはmyuserひとりだけ（簡略化）"""
+    return User('myuser', 'mypassword')
